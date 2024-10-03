072e25acdd3fcf22c03378f3fef10ad9047a5352
Finlay <finlay.gress@gmail.com>
2024-10-03 03:52:39 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 sudo dnf check-update
 sudo dnf config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 sudo dnf install docker-ce docker-ce-cli containerd.io docker-compose-plugin git
