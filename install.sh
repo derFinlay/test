c0d9e7fc51457e764d64ec060b8150679ab43ad8
Finlay <finlay.gress@gmail.com>
2024-10-03 03:50:00 +0200

    wada

--- install.sh
+++ install.sh
@@ -1,7 +1,10 @@
 sudo dnf check-update
 sudo dnf config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-sudo dnf install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+sudo dnf install docker-ce docker-ce-cli containerd.io docker-compose-plugin git
 sudo systemctl start docker
 sudo systemctl enable docker
 docker version
 docker compose version
+git clone https://github.com/derFinlay/test.git
+cd test/obsidian
+docker compose up -d 
