087d0cd0751d14cd3bd4ac65d193b30d42223aeb
Finlay <finlay.gress@gmail.com>
2024-10-03 18:23:49 +0200

    Added install script comments

--- install.sh
+++ install.sh
@@ -1,17 +1,26 @@
 #!/bin/bash
 
+#update system and install docker, compose and git
 sudo dnf check-update
 sudo dnf config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 sudo dnf install docker-ce docker-ce-cli containerd.io docker-compose-plugin git
+
+#enable docker (autostart)
 sudo systemctl start docker
 sudo systemctl enable docker
 docker version
 docker compose version
+
+#clone setup repo
 mkdir setup
 git clone https://github.com/derFinlay/test.git setup
 cd setup
+
+#Clone DDNS tool
 mkdir temp
 git clone https://github.com/derFinlay/cloudflare-ddns-updater temp
 mv temp/* ddns
 rm -rf temp
+
+#Start all Systems
 docker compose up -d
